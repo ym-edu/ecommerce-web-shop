4a911b2a6fb37306000f4701be815f683e4549c8
nym <nym@nym.local>
2021-01-15 14:59:07 -0600

    created products component and tested product data

---
+++ src/components/Products.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+
+import { products } from '../constants/products'
+// import Product from './Product'
+
+export default function Products() {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}> {/* material UI grid */}
+          {products.map(product => {
+            return (
+            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+              <h1>{product.name}</h1> {/* Test data */}
+            </Grid>
+              )
+          })}
+      </Grid>
+    </main>
+  )
+}
