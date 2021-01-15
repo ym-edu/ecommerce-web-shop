2132c6e48344bf269896a5c51a91b7448778555a
nym <nym@nym.local>
2021-01-15 14:10:51 -0600

    created mock products array

---
+++ src/constants/products.js
@@ -0,0 +1,6 @@
+export const products = [
+  { id: 1, name: 'Miel 1Kg', description: 'Frasco pequeño de miel organica.' },
+  { id: 2, name: 'Miel 6Kg', description: 'Frasco grande de miel organica.' },
+  { id: 3, name: 'Miel 27Kg', description: 'Cubeta chica de miel organica.' },
+  { id: 4, name: 'Shampoo', description: 'Honney Shampoo.' },
+];
