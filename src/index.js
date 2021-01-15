16bfae793270fee5b78b043dd45e3a812b38eef6
nym <nym@nym.local>
2021-01-15 13:50:44 -0600

    created App file in src

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App';
+
+export default function index() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
